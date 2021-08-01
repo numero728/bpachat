a927134fc4fb1b99bbc1f7acd674fdff2b910707
numero728 <numero728@naver.com>
2021-08-01 17:13:04 +0900

    dgdg

--- run.py
+++ run.py
@@ -7,34 +7,6 @@ from datetime import datetime
 from urllib.parse import unquote
 import sys
 
-def log(now_date,message):
-    cur_date=re.search('([\d]+)',re.sub('-','',now_date)).group(1)
-    try:
-        with open(os.path.join('log',f'{cur_date}.txt'),'a') as fp:
-            fp.write(message+'\n')
-    except Exception as e:
-        print(e)
-        with open(os.path.join('log',f'{cur_date}.txt'),'w') as fp:
-            fp.write(message+'\n')
-
-def log_read(now_date):
-    cur_date=re.search('([\d]+)',re.sub('-','',now_date)).group(1)
-    data=list()
-
-    try:
-        with open(os.path.join('log',f'{cur_date}.txt'),'r') as fp:
-            log_text=fp.readlines()
-        log_text=[text for text in log_text if '[Send]' in text]
-        for ln in log_text:
-            try:
-                data.append((re.search('\[Send\] ([\w]+) ',ln).group(1), re.search('msg:([\w\d %]+) timestamp',ln).group(1)))
-            except:
-                continue
-    except:
-        data.append(('System','오늘의 첫 메시지입니다.'))
-    return data
-            
-    
 # app 호출
 if True:
     app = Flask(__name__)
@@ -48,26 +20,19 @@ if True:
     
     @socketio.on('connect')
     def connect():
-        # user=request.args.get('user')
-        now=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        user='Who'
-        log_msg=(f'[Connected] {user} timestamp:{now}')
-        log(now,log_msg)
-        # cmd='echo 111 > /home/khk/test'
-        # os.system(cmd)
-    # @socketio.on('login')
-    # def login(user):
-        
+       pass
 
-    # @socketio.on('c_msg')
-    # def c_msg(data):  # 요기서 댓글 텍스트 처리
-    #     user=data['user']
-    #     msg=data['msg']
-    #     now=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    #     log_msg=(f'[Send] {user} msg:{msg} timestamp:{now}')
-    #     emit('s_msg', {'user': user, 'msg': msg}, broadcast=True)
-    #     log(now,unquote(log_msg))
-    #     flash("전화 받으세요")
+    @socketio.on('login')
+    def login(data):
+        user=data['user']
+        print(user)
+        
+    @socketio.on('c_msg')
+    def c_msg(data):
+        user=data['user']
+        msg=data['msg']
+        now=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        emit('s_msg', {'user': user, 'msg': msg}, broadcast=True)
 
     # @socketio.on('disconnect')
     # def disconnect():
@@ -78,15 +43,12 @@ if True:
 @app.route('/')
 def chat():
     now=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    texts=log_read(now)
-    print(texts)
-    return render_template('index.html',texts=texts)
+    return render_template('index.html')
 
 @app.route('/log')
 def logpage():
     now=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    texts=log_read(now)
-    return texts
+    return now
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
