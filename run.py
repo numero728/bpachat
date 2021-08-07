b7177b6d8a61a066d10f88a8f5bf579a4dc5fe83
numero728 <numero728@naver.com>
2021-08-07 17:13:54 +0900

    fa

--- run.py
+++ run.py
@@ -74,14 +74,9 @@ def drive():
 @app.route('/upload',methods=['POST'])
 def upload():
     file=request.files['file']
-    return app.config['UPLOAD_FOLDER']
-    #file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename))
-    # return redirect('/drive')
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename))
+    return redirect('/drive')
 
-@app.route('/oslist')
-def oslist():
-    lst=str(os.listdir(os.getcwd()))
-    return lst
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
