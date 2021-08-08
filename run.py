447ed3f3acddb5addfc5143949f7b4170760d895
numero728 <numero728@naver.com>
2021-08-08 19:29:15 +0900

    fa

--- run.py
+++ run.py
@@ -75,7 +75,7 @@ def drive():
 def upload():
     file=request.files['file']
     file_path= os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
-    file.save(file_path))
+    file.save(file_path)
     return redirect('/drive')
 
 
