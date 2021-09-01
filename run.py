fed3227543bc77b3d354505072491bd8729c855d
numero728 <numero728@naver.com>
2021-09-01 23:50:15 +0900

    af

--- run.py
+++ run.py
@@ -77,9 +77,9 @@ def upload():
     file=request.files['file']
     file_name=request.form['file_name']
     # file_path= ascii(os.path.join(app.config['UPLOAD_FOLDER'],file_name))
-    # file_path= ascii(file_name)
-    file.save(os.path.join('.',file_name))
-    # raise Exception(str(os.path.abspath(os.getcwd())))
+    file_path= ascii(file_name)
+    file.save(os.path.join('.',file_path))
+    raise Exception(str(os.path.abspath(os.getcwd())))
     return redirect('/drive')
 
 
